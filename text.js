bbae49e9faa636cb328acc6f437ab19c7e632254
Farllyn <125272959+farllyn007@users.noreply.github.com>
2024-08-22 11:51:22 -0300

    l

---
+++ text.js
@@ -0,0 +1,17 @@
+function verificarResposta(silaba) {
+    if (silaba === 'la') {
+        document.getElementById('resultado').innerText = 'Resposta correta! Parabéns!';
+    } else {
+        document.getElementById('resultado').innerText = 'Resposta errada. Tente novamente.';
+    }
+}
+
+
+
+function verificarResposta(silaba) {
+    if (silaba === 'da') {
+        document.getElementById('resultado').innerText = 'Resposta correta! Parabéns!';
+    } else {
+        document.getElementById('resultado').innerText = 'Resposta errada. Tente novamente.';
+    }
+}
